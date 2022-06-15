35e9097c4b152dc5e3dd0023135383ab13ae9aa6
Minh <Kamengoku18@gmail.com>
2022-06-16 02:43:42 +0700

    first commit

---
+++ Eleaning_Web/Model/DBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Eleaning_Web.Model
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Class> Class { get; set; }
+        public DbSet<Answer> Answer { get; set; }
+        public DbSet<Document> Document { get; set; }
+        public DbSet<Exam> Exam { get; set; }
+        public DbSet<ResultExam> ResultExam { get; set; }
+        public DbSet<LearningResult> LearningResult { get; set; }
+        public DbSet<Question> Question { get; set; }
+        public DbSet<Schedule> Schedule { get; set; }
+        public DbSet<Test> Test { get; set; }
+        public DbSet<Subject> Subject { get; set; }
+        public DbSet<TestSchedule> TestSchedule { get; set; }
+        public DbSet<Role> Role { get; set; }
+        public DbSet<ContentTest> ContentTest { get; set; }
+       
+    }
+}
---
+++ Eleaning_Web/Services/UserRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Eleaning_Web.Services
+{
+    public class UserRequest
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required, MinLength(6, ErrorMessage = "Please enter at least 6 characters!!!")]
+        public string Password { get; set; } = string.Empty;
+        [Required, Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+        public int RoleId { get; set; }
+
+    }
+}
